916ad7ef234362a3a148a3540f65be8d30cea860
mateus <mateusreis@gmail.com>
2025-03-21 13:59:23 -0300

    pagina funcional

---
+++ patterns/message-404.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Title: 404 message custom-blocks-demo
+ * Slug: custom-blocks-demo/message-404
+ * Categories: 
+ * Block Types: 
+ * Description: 
+ *
+ * @package WordPress
+ * @subpackage 
+ * @since 
+ */
+
+?>
+
+<div class="wp-block-group alignfull">
+    <H1>EU SOU O PATTERN 404</H1>
+</div>
--- woocommerce/single-product/price.php
+++ woocommerce/single-product/price.php
@@ -23,5 +23,6 @@ global $product;
 
 ?>
 
-<HR>
+<HR>uuuyyyuuu
+<HR>uuuyyyuuu
 <p class="<?php echo esc_attr( apply_filters( 'woocommerce_product_price_class', 'price' ) ); ?>"><?php echo $product->get_price_html(); ?></p>
