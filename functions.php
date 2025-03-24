a7ada7de3cd1ed8d8856005b7d36d20adaf62367
mateus <mateusreis@gmail.com>
2025-03-24 11:16:43 -0300

    refactor

--- functions.php
+++ functions.php
@@ -6,7 +6,7 @@
  * @since 1.0.0
  */
 
- remove_action( 'woocommerce_shop_loop_item_title','woocommerce_template_loop_product_title', 10 );
+
 
 if ( ! function_exists( 'custom_blocks_demo_setup' ) ) {
 	/**
@@ -118,47 +118,11 @@ add_action( 'wp_enqueue_scripts', 'custom_blocks_demo_enqueue_styles' );
 add_action( 'enqueue_block_editor_assets', 'custom_blocks_demo_enqueue_styles' );
 
 // Declare WooCommerce support.
-add_theme_support( 'woocommerce' );
-add_theme_support( 'wc-product-gallery-zoom' );
-add_theme_support( 'wc-product-gallery-lightbox' );
-add_theme_support( 'wc-product-gallery-slider' );
+// imporatnte para o tema?
+// add_theme_support( 'woocommerce' );
+// add_theme_support( 'wc-product-gallery-zoom' );
+// add_theme_support( 'wc-product-gallery-lightbox' );
+// add_theme_support( 'wc-product-gallery-slider' );
 
 
 
-// add_action('woocommerce_shop_loop_item_title', 'abChangeProductsTitle', 10 );
-// function abChangeProductsTitle() {
-//     echo '<h2 class="woocommerce-loop-product_title">xoopoop<a href="'.get_the_permalink().'">' . get_the_title() . '</a></h2>';
-// }
-
-
-// remove_action( 'woocommerce_template_single_title','woocommerce_template_loop_product_title', 10 );
-// add_action('woocommerce_template_single_title', 'chagnesingletiotle', 10 );
-// function chagnesingletiotle() {
-//     echo '<h2 class="woocommerce-loop-product_title">xoopoop<a href="'.get_the_permalink().'">' . get_the_title() . '</a></h2>';
-// }
-
-
-
-// add_filter('the_title', 'your_shop_custom_title', 20, 2);
-
-// function your_shop_custom_title($the_title, $id)
-// {
-
-//   if (is_shop() && get_post_type($id) == 'product') : // runs only on the shop page
-
-//     $the_title = 'xiixii<a href="' . get_the_permalink() . '">' . $the_title . '</a>';
-
-//   endif;
-
-//   return $the_title;
-// }
-
-
-
-// nao funciona se tem template html 
-add_action('wp_head', 'shop_title_override', 9999999999999);
-
-function shop_title_override() {
-   remove_action( 'woocommerce_shop_loop_item_title', 'woocommerce_template_loop_product_title', 10 );
-   remove_action( 'woocommerce_shop_loop_item_title', 'x_woocommerce_template_loop_product_title', 10 );
-}
